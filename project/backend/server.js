9b35ca4836fb7336cd5dbbc70c7010f13804bc69
niranjna2024-hub <niranjna.2024@vitstudent.ac.in>
2026-03-30 22:00:38 +0530

    Update server.js

--- project/backend/server.js
+++ project/backend/server.js
@@ -19,6 +19,8 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Backend working" });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
